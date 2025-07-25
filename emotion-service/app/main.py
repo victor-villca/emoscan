56839b4aa5b08bfaaa0199355c973ec37c586bcc
victor-villca <villcavictorleon@gmail.com>
2025-07-25 19:51:30 -0400

    feat:ai emotion service model endpoint

--- emotion-service/app/main.py
+++ emotion-service/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.routes import predict
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="EmoScan Emotion Analysis API",
     description="Detect emotions from face images using a trained deep learning model",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(predict.router, prefix="/api")
